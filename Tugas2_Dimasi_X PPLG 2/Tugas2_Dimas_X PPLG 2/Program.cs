f813e2d319399bdb52aab59f1ab80f13375df790
dimas258749-lgtm <dimas258749@student.smkn1kandeman.sch.id>
2025-10-21 15:17:42 +0700

    Add files via upload

---
+++ Tugas2_Dimasi_X PPLG 2/Tugas2_Dimas_X PPLG 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas2_Danu_Tri_X_PPLG_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai pertama: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukkan nilai kedua: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Masukkan nilai ketiga: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = rata_rata(a, b, c);
+            Console.WriteLine($"\nrata-rata = {hasil}");
+        }
+
+        static double rata_rata(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ Tugas3_Dimas_ XPPLG 2/Tugas3_Dimas_XPPLG 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas3_Danu_Tri__XPPLG_2
+{
+    internal class Program
+    {
+        /* buat Fungsikonversi untuk mengubah suhu dari celcius ke fahrenheit dengan
+             F = (C x 9/5) + 32
+            Tampilkan hasil konversi pada program utama! */
+
+        static double ConvertCelciusToFahrenheit(double celcius)
+        {
+            double fahrenheit = (celcius * 9 / 5) + 32;
+            return fahrenheit;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program konversi suhu dari ke fahrenheit");
+            Console.Write("Masukan suhu dalam celcius: ");
+            double celcius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{celcius}°C= {fahrenheit}");
+        }
+    }
+}
